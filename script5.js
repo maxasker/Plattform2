08dfc5b8dd24bc3ef470ad11940a0bc24fefe797
maxasker <maxasker@hotmail.com>
2017-04-05 02:56:57 +0200

    hej

--- script5.js
+++ script5.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
     var data2 = $.parseJSON(data);
     if (data2.success == true){
         console.log("YAY")
-        getmedia();
     };
     
     });
@@ -50,17 +49,17 @@ function updatemedia(data){
     $("#showmedia").empty();
     for (var i = 0; i < data["files"].length; i++){
         if (data["files"][i]["type"] == "photo"){
-    $('<div class="media col-md-6 col-xs-12 col-sm-6 col-lg-3"> <img class="img-responsive mediaphoto" src="' + data["files"][i]["path"] + '"></div>').appendTo($('#showmedia'));
+    $('<div class="media col-md-6 col-xs-12 col-sm-6 col-lg-3"><h2>'+data["files"][i]["title"]+'</h2> <img class="img-responsive mediaphoto" src="' + data["files"][i]["path"] + '"></div>').appendTo($('#showmedia'));
         }
     }
     for (var i = 0; i < data["files"].length; i++){
         if (data["files"][i]["type"] == "video"){
-    $('<div class="media img-thumbnail"> <video controls type="video/mp4" class="mediavideo" src="' + data["files"][i]["path"] + '"></video></div>').appendTo($('#showmedia'));
+    $('<div class="media img-thumbnail"><h2>'+data["files"][i]["title"]+'</h2><video controls type="video/mp4" class="mediavideo" src="' + data["files"][i]["path"] + '"></video></div>').appendTo($('#showmedia'));
         }
     }
     for (var i = 0; i < data["files"].length; i++){
         if (data["files"][i]["type"] == "audio"){
-    $('<div class="media col-md-6 col-xs-12 col-sm-6 col-lg-3"> <audio controls class="mediaaudio" src="' + data["files"][i]["path"] + '"></audio></div>').appendTo($('#showmedia'));
+    $('<div class="media col-md-6 col-xs-12 col-sm-6 col-lg-3"><h2>'+data["files"][i]["title"]+'</h2> <audio controls class="mediaaudio" src="' + data["files"][i]["path"] + '"></audio></div>').appendTo($('#showmedia'));
         }
     }
 }
